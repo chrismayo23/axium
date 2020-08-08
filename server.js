d0abe580c3adec08a1b427a4328728f200192582
ccm <chrismayo23@gmail.com>
2020-08-08 18:38:34 -0400

    updates

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-table'));
+app.use(express.static(__dirname + '/dist/axium'));
 
 app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname + '/dist/angular-table/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/axium/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
