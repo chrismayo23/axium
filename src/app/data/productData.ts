077893f6046896ab6ceb5437e8897afef1d83f9b
ccm <chrismayo23@gmail.com>
2020-08-09 08:14:04 -0400

    action bar updates

--- src/app/data/productData.ts
+++ src/app/data/productData.ts
@@ -1,40 +1,40 @@
 export const ProductData = {
-  name: "Tough Mojo Jacket",
-  img_one_url: "../assets/tough-mojo-jacket-img1.png",
-  img_two_url: "../assets/tough-mojo-jacket-img2.png",
+  name: 'Tough Mojo Jacket',
+  img_one_url: '../assets/tough-mojo-jacket-img1.png',
+  img_two_url: '../assets/tough-mojo-jacket-img2.png',
   descriptors: [
-    "300 GRAMS OF PRIMALOFT ONE INSULATION",
-    "SCHOELLER MICROFIBER SHELL",
-    "DIAGONAL QUILTING",
-    "MICROFIBER RIPSTOP LINER",
-    "ZIPPER GARAGE AT COLLAR",
-    "ANCHORED BOTTOM-HEM ADJUSTERS",
-    "3-IN-1 FRONT POCKETS",
-    "ZIPPERED CHEST POCKET",
-    "TWO INTERIOR ZIPPERED POCKETS",
-    "MADE IN PHILIPPINES"
+    '300 GRAMS OF PRIMALOFT ONE INSULATION',
+    'SCHOELLER MICROFIBER SHELL',
+    'DIAGONAL QUILTING',
+    'MICROFIBER RIPSTOP LINER',
+    'ZIPPER GARAGE AT COLLAR',
+    'ANCHORED BOTTOM-HEM ADJUSTERS',
+    '3-IN-1 FRONT POCKETS',
+    'ZIPPERED CHEST POCKET',
+    'TWO INTERIOR ZIPPERED POCKETS',
+    'MADE IN PHILIPPINES'
   ],
   descriptions: [
-    "Our most insulated jacket to date, the city-styled Highline is designed to keep you warm during the deep freeze of winter.",
-    "With 300 grams of PrimaLoft ONE insulation, the Highline offers incredible heat retention while remaining form fitting and low volume - essentially all the warmth of a puffer jacket without all the puff.  Its outer shell is made from a water-resistant  Schoeller  microfiber fabric that has been diagonally quilted, while its inside liner is made from a silky-soft microfiber ripstop nylon",
-    "The Highline features a zipper garage to prevent a cold metal zipper from pressing against your neck, non-quilted side panels and underarms for a sleeker fit, and anchored bottom-hem adjusters for a snug fit. It has two uniquely designed 3-in-1 front pockets, which include a top-zippered entrance, a drop pocket with donut snap, and a side hand-warmer. Additional storage can be found in its exterior chest pocket and two interior zippered pockets."
+    'Our most insulated jacket to date, the city-styled Highline is designed to keep you warm during the deep freeze of winter.',
+    'With 300 grams of PrimaLoft ONE insulation, the Highline offers incredible heat retention while remaining form fitting and low volume - essentially all the warmth of a puffer jacket without all the puff.  Its outer shell is made from a water-resistant  Schoeller  microfiber fabric that has been diagonally quilted, while its inside liner is made from a silky-soft microfiber ripstop nylon',
+    'The Highline features a zipper garage to prevent a cold metal zipper from pressing against your neck, non-quilted side panels and underarms for a sleeker fit, and anchored bottom-hem adjusters for a snug fit. It has two uniquely designed 3-in-1 front pockets, which include a top-zippered entrance, a drop pocket with donut snap, and a side hand-warmer. Additional storage can be found in its exterior chest pocket and two interior zippered pockets.'
   ],
   reviews: [
     {
-      review_name: "Shaun White",
-      review_title: "This Thing is a monster!",
-      img_photo_url: "../assets/reviewer1.png",
+      review_name: 'Shaun White',
+      review_title: 'This Thing is a monster!',
+      img_photo_url: '../assets/reviewer1.png',
       stars: 4,
-      review_date: "08/30/2013",
-      review_description: "I picked up this jacket for use in San Francisco and the surrounding Bay Area (Lake Tahoe included). I use this jacket for everything. It is often cloudy/drizzly/cold in SF and this jacket deals with it all. I have not tried in heavy rain, but in drizzly conditions, the DWR treatment definitely makes small drops bead right off. It also has a nice fleece-like liner inside and keeps me really ward down to 40 degrees or so. In Tahoe, I have used this with a shirt or base layer, mid layer fleece, and then this jacket. I don't get cold too easily but in temps 20's - 30's I felt great (with some gloves and a beanie of course.) I have not tried it skiing yet because I snowboard and to be honest it wouldn't be quite large enough (I like baggier snowboard clothing). But I have seen people who wear softshells skiing all the time. I also use this when I go to work and want a jacket that dresses up nice. With slacks, nice shoes, and a button up it looks great. I am 6' and about 190 lbs (34 in arms) and the large fits perfect. THe pockets are well designed as well. The soft shell is the new 'do-everything' jacket: wind/water resistent but also maleable and comfy for wearing anywhere. I highly recommend this jacket."
+      review_date: '08/30/2013',
+      review_description: 'I picked up this jacket for use in San Francisco and the surrounding Bay Area (Lake Tahoe included). I use this jacket for everything. It is often cloudy/drizzly/cold in SF and this jacket deals with it all. I have not tried in heavy rain, but in drizzly conditions, the DWR treatment definitely makes small drops bead right off. It also has a nice fleece-like liner inside and keeps me really ward down to 40 degrees or so. In Tahoe, I have used this with a shirt or base layer, mid layer fleece, and then this jacket. I don\'t get cold too easily but in temps 20\'s - 30\'s I felt great (with some gloves and a beanie of course.) I have not tried it skiing yet because I snowboard and to be honest it wouldn\'t be quite large enough (I like baggier snowboard clothing). But I have seen people who wear softshells skiing all the time. I also use this when I go to work and want a jacket that dresses up nice. With slacks, nice shoes, and a button up it looks great. I am 6\' and about 190 lbs (34 in arms) and the large fits perfect. The pockets are well designed as well. The soft shell is the new \'do-everything\' jacket: wind/water resistent but also maleable and comfy for wearing anywhere. I highly recommend this jacket.'
     },
     {
-      review_name: "Jennie Loren",
-      review_title: "Luv it so much I bought 2!",
-      img_photo_url: "../assets/reviewer2.png",
+      review_name: 'Jennie Loren',
+      review_title: 'Luv it so much I bought 2!',
+      img_photo_url: '../assets/reviewer2.png',
       stars: 3,
-      review_date: "01/12/2013",
-      review_description: "Loved this jacket so much from previous years I decided to buy a second in a different color. I'm tall and fairly slender and its tailored silhouette fits me nicely. For perspective I'm around 6'3 ~150 lbs. and a large does the trick. I'll wear this for all of fall and into the winter. Despite not being bulky or baggy I find it good and warm for anything down to 40 degrees and perhaps lower if you're keeping up on your layering. For weather at freezing or below you should consider something heavier."
+      review_date: '01/12/2013',
+      review_description: 'Loved this jacket so much from previous years I decided to buy a second in a different color. I\'m tall and fairly slender and its tailored silhouette fits me nicely. For perspective I\'m around 6\'3 ~150 lbs. and a large does the trick. I\'ll wear this for all of fall and into the winter. Despite not being bulky or baggy I find it good and warm for anything down to 40 degrees and perhaps lower if you\'re keeping up on your layering. For weather at freezing or below you should consider something heavier.'
     }
   ]
 }
