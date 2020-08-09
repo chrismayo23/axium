2969e8d7dd1dd23c4b0e87648ebc3871fa71df0f
ccm <chrismayo23@gmail.com>
2020-08-09 09:32:14 -0400

    spelling

--- src/app/data/menuData.ts
+++ src/app/data/menuData.ts
@@ -6,7 +6,7 @@ export const MenuData = {
     {
       menuItemName: 'Clothing',
       subMenuItem: [
-        'Outerweart',
+        'Outerwear',
         'Hiking Shoes/Boots',
         'Gloves',
         'Hats'
